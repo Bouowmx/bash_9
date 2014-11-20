d017a085e5cc28a2a1e0894602993bef74cd17ec
Sean Yip <bouowmx@gmail.com>
2014-11-20 11:02:33 -0500

    Inital commit 4

--- bash_9.c
+++ bash_9.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,12 +7,21 @@
 
 int main() {
 	printf("Welcome to Bash ⑨. Authored by Cirno. Expect the strongest and the best.\n");
-	char * computer_name = (char *) malloc(HOST_NAME_MAX * sizeof(char) + 1);
-	gethostname(computer_name, sizeof(computer_name));
-	chdir("~"); //Change current directory to home, or if it cannot, the current directory remains the directory of the executable.
-	char * current_directory = getcwd(current_directory, PATH_MAX);
+	char computer_name[HOST_NAME_MAX + 1];
+	gethostname(computer_name, sizeof(computer_name) / sizeof(char));
+	printf("%s\n", getenv("HOME"));
+	chdir(getenv("HOME")); //Change current directory to home, or if it cannot, the current directory remains the directory of the executable.
+	//printf("Error %i: %s\n", errno, strerror(errno));
+	char current_directory[PATH_MAX + 1];
+	current_directory = getcwd(current_directory, PATH_MAX);
 	printf("%s@%s:%s$", getlogin(), computer_name, current_directory);
+	printf("\n");
 	free(current_directory);
-	
+	/*
+	while (1) {
+		char input[1024 + 1];
+		fgets(input, sizeof(input) / sizeof(char), stdin);
+		char
+	*/
 	return 0;
 }
