f288e3fe8c4b80d84ae92eb6d1008bd76cbec3bc
Bouowmx <bouowmx@gmail.com>
2014-11-19 22:30:49 -0500

    Inital commit 3

--- bash_9.c
+++ bash_9.c
@@ -8,8 +8,8 @@ int main() {
 	printf("Welcome to Bash ⑨. Authored by Cirno. Expect the strongest and the best.\n");
 	char * computer_name = (char *) malloc(HOST_NAME_MAX * sizeof(char) + 1);
 	gethostname(computer_name, sizeof(computer_name));
-	chdir("~");
-	char * current_directory = getcwd(current_directory, NULL);
+	chdir("~"); //Change current directory to home, or if it cannot, the current directory remains the directory of the executable.
+	char * current_directory = getcwd(current_directory, PATH_MAX);
 	printf("%s@%s:%s$", getlogin(), computer_name, current_directory);
 	free(current_directory);
 	
