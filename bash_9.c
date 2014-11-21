5dd0341657dde9baf1e367693e7eaf8935c6f538
Sean Yip <bouowmx@gmail.com>
2014-11-21 13:18:47 -0500

    Remove leading whitespace

--- bash_9.c
+++ bash_9.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <limits.h>
 #include <pwd.h>
@@ -11,24 +12,22 @@ int main() {
 	char * user;
 	char computer_name[HOST_NAME_MAX];
 	char current_directory[PATH_MAX];
-	//chdir(getenv("HOME"));
 	char input[1024];
+	char * input_2;
+	int i = 0;
 	while (1) {
 		user = getpwuid(getuid())->pw_name;
 		gethostname(computer_name, sizeof(computer_name) / sizeof(char));
 		getcwd(current_directory, PATH_MAX);
 		printf("%s@%s:%s⑨ ", user, computer_name, current_directory);
 		fgets(input, sizeof(input) / sizeof(char), stdin);
-		//if (strchr(input, '\n') != NULL) {*strchr(input, '\n') = NULL;}
+		input_2 = strdup(input);
+		
 		printf("Input: %s\n", input);
 		if ((input[0] == 'c') && (input[1] == 'd')) {
-			printf("Changing directory.\n");
-			char * input_2 = strdup(input);
+			input_2 = strdup(input);
 			strsep(&input_2, " ");
-			char * directory = strsep(&input_2, " \n");
-			printf("Changing to: \"%s\"\n", directory);
-			chdir(directory);
-			printf("Error %i: %s\n", errno, strerror(errno));
+			chdir(strsep(&input_2, " \n"));
 		}
 		if (strcmp(input, "exit") == 0) {return 0;}
 	}
