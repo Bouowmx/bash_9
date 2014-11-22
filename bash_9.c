145bc273adedb9ecd7617b959065ab51695be38f
Bouowmx <bouowmx@gmail.com>
2014-11-21 19:04:39 -0500

    Fork this

--- bash_9.c
+++ bash_9.c
@@ -14,29 +14,49 @@ int main() {
 	char current_directory[PATH_MAX];
 	char input[1024];
 	char * input_2;
-	int i, j;
+	int i;
 	while (1) {
 		user = getpwuid(getuid())->pw_name;
 		gethostname(computer_name, sizeof(computer_name) / sizeof(char));
 		getcwd(current_directory, PATH_MAX);
 		printf("%s@%s:%s⑨ ", user, computer_name, current_directory);
 		fgets(input, sizeof(input) / sizeof(char), stdin);
+		//Remove leading and trailing whitespace
 		input_2 = strdup(input);
-		char * input_2_pointer = input_2; //Used to free later
-		for (i = 0, j = 0; j < strlen(input_2); i++) {
-			if (isspace(input_2[j])) {j++;}
-			else {input[i++] = input_2[j++];}
+		for (i = 0; i < strlen(input_2); i++) {
+			if (!(isspace(input_2[i]))) {
+				strcpy(input, &input_2[i]);
+				break;
+			}
 		}
-		//Remove trailing		
+		free(input_2);
+		input_2 = strdup(input);
+		for (i = strlen(input_2) - 1; i >= 0; i--) {
+			if (!(isspace(input_2[i]))) {
+				strncpy(input, input_2, i + 1);
+				break;
+			}
+		}
+		free(input_2);		
 		printf("Input: %s\n", input);
 		if ((input[0] == 'c') && (input[1] == 'd')) {
 			input_2 = strdup(input);
 			strsep(&input_2, " ");
 			chdir(strsep(&input_2, " \n"));
-			if (errno != 0) {printf("%s\n", strerror(errno));}
+			if (errno != 0) {printf("%s\n", strerror(errno));} //Strangely, building and running with Cygwin will cause this to print "Invalid or incomplete multibyte or wide character". Changing directories is not affected.
 		}
-		if (strcmp(input, "exit") == 0) {return 0;}
+		else if (strcmp(input, "exit") == 0) {return 0;}
+		else {
+			printf("Forking.\n");
+			pid_t pid = fork();
+			if (pid > 0) {wait(pid);}
+			else {
+				execlp(input, input, (char *) NULL);
+				printf("%s\n", strerror(errno));
+			}
+		}
 	}
-	return 0;
+	
+	return EXIT_SUCCESS;
 }
 
