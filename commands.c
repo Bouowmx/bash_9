95e0e7d0f115ad051218a9b6ab1004d74e5012c1
Sean Yip <bouowmx@gmail.com>
2014-11-24 13:19:58 -0500

    Number of semicolons in a string? Nope.

--- commands.c
+++ commands.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+char ** split(char * string, char delimiter) { //Just like Python's split. Returns a null-terimated array.
+	//Find the number of delimiters in string. The size of the array needed to hold the split string is the number of delimiters plus two (including the terminating null).
+	int i;
+	char * string_2;
+	for (string_2 = string, i = 0; strchr(string_2, delimiter) != NULL; string_2++, i++) {printf("%i: %s\n", i, string_2);}
+	printf("%i\n", i);
+	return NULL;
+}
+
 char * trim(char * string) { //Modifies in-place.
 	char * string_2;
 	int i;
@@ -32,11 +41,14 @@ int main() {
 	char * commands[1024];
 	int i;
 	
+	printf("Enter input: ");
 	fgets(input, sizeof(input) / sizeof(char), stdin);
 	printf("Input: %s\n", input);
-	input_2 = strdup(input);
+	/*input_2 = strdup(input);
 	for (command = strsep(&input_2, ";"), i = 0; command != NULL; command = strsep(&input_2, ";"), i++) {commands[i] = trim(command);}
-	for (; i >= 0; i--) {printf("%i: %s\n", i, commands[i]);}
+	for (; i >= 0; i--) {printf("%i: %s\n", i, commands[i]);}*/
+	
+	split(input, ';');
 	
 	return EXIT_SUCCESS;
 }
