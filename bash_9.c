9fcff6bb306861eb8a63f448b7d18ed58b0f2b3a
Sean Yip <bouowmx@gmail.com>
2014-11-25 13:19:05 -0500

    Arguments

--- bash_9.c
+++ bash_9.c
@@ -10,18 +10,17 @@
 char ** split(char *, char *);
 char * trim(char *);
 
-char ** split(char * string, char * delimiters) { //Just like Python's split. Returns a null-terimated array.
+char ** split(char * string, char * delimiters) { //Similar to Python's split. Returns a null-terimated array with each element trimmed of whitespace.
 	//Find the number of delimiters in string. The size of the array needed to hold the split string is the number of delimiters plus two (including the terminating null).
 	//About the increments: prefix will increment first and use that value, and suffix will use the current value and then increment.
 	int i;
 	char * string_2;
 	for (string_2 = string, i = 0; string_2 != NULL; string_2 = strpbrk(++string_2, delimiters), i++) {}
-	char * string_3
 	char ** tokens = (char **) malloc(++i * sizeof(char *));
 	char * token;
 	int j;
-	for (string_3 = string_2 = strdup(string), token = strsep(&string_2, delimiters), j = 0; j < i; token = strsep(&string_2, delimiters), j++) {tokens[j] = trim(token);}
-	free(string_3);
+	for (string_2 = strdup(string), token = strsep(&string_2, delimiters), j = 0; j < i; token = strsep(&string_2, delimiters), j++) {tokens[j] = (token != NULL) ? trim(token) : token;}
+	free(string_2);
 	return tokens;
 }
 	
@@ -51,8 +50,6 @@ char * trim(char * string) { //Modifies in-place.
 
 int main() {
 	printf("Welcome to Bash ⑨. Authored by Cirno. Expect the strongest and the best.\n");
-	char * command;
-	char * commands[1024];
 	char computer_name[HOST_NAME_MAX];
 	char current_directory[PATH_MAX];
 	char input[1024];
@@ -77,8 +74,12 @@ int main() {
 		}
 		else if (strcmp(input, "exit") == 0) {return 0;}
 		else {
+			input_2 = strdup(input);
+			char * command = strsep(&input_2, " ");
+			char ** arguments = split(input_2, " ");
+			//for (
 			pid_t pid = fork();
-			if (!pid) {execlp(input, input, (char *) NULL);} //Strangely, using the condition "pid != 0" will cause incorrect forking. Additionally, a new line in input will result in "No such file or directory". Finally, entering a non-valid command will cause the child process to continue living.
+			if (!pid) {execvp(command, arguments);} //Strangely, using the condition "pid != 0" will cause incorrect forking. Additionally, a new line in input will result in "No such file or directory". Finally, entering a non-valid command will cause the child process to continue living.
 			else {wait(NULL);}
 		}
 		printf("%s\n", strerror(errno));
